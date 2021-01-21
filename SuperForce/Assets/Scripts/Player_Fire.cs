73740c59c66c5249a3cf1ad71a86b7beb9f1225d
jacob7123 <h39713971@gmail.com>
2021-01-21 15:07:11 -0800

    add grenade feature

---
+++ SuperForce/Assets/Scripts/Player_Fire.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Fire : MonoBehaviour
+{
+    public GameObject Bullet;
+    public GameObject Fire_Partical;
+    public AudioClip Fire_Sound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(Bullet, transform.position, transform.rotation);
+            Instantiate(Fire_Partical, transform.position, transform.rotation);
+            GetComponent<AudioSource>().PlayOneShot(Fire_Sound);
+        }
+    }
+}
---
+++ SuperForce/Assets/Scripts/Player_bullet_move.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_bullet_move : MonoBehaviour
+{
+    public float MoveSpeed = 30;
+    public float Destroy_Time = 1.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, Destroy_Time);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0 * Time.deltaTime, 0 * Time.deltaTime, MoveSpeed * Time.deltaTime);
+    }
+}
